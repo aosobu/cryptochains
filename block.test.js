666e847bf0c5e9f4c964e06849147eceb49a1ccf
Adewale Osobu <aosobu@teamapt.com>
2021-01-09 13:46:37 +0100

    creates blockchain class and blockchain test class

---
+++ block.test.js
@@ -0,0 +1,31 @@
+const Block = require('./block');
+const { GENESIS_DATA } = require('./config');
+
+describe('Block', ()=>{
+
+    const timestamp = new Date();
+    const lastHash = 'foo-lastHash';
+    const hash = 'hash';
+    const data = ['blockchain', 'data'];
+    const block = new Block({timestamp, lastHash, hash, data});
+
+    it('has a timestamp, lastHash, hash, and data property', () => {
+        expect(block.timestamp).toEqual(timestamp);
+        expect(block.lastHash).toEqual(lastHash);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
+    });
+
+    describe('genesis()' , () => {
+        const genesisBlock = Block.genesis();
+
+        it('returns a block instance', () => {
+            expect(genesisBlock instanceof Block).toBe(true);
+        })
+
+        it('returns the gensis data', () => {
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        })
+    })
+
+}); 
---
+++ config.js
@@ -0,0 +1,10 @@
+
+//the genesis data contains the default data for the genesis block
+const GENESIS_DATA = {
+    timestamp: new Date(),
+    lastHash: '-----',
+    hash: 'hash-one',
+    data: []
+};
+
+module.exports = { GENESIS_DATA }
